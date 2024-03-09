83042accc76ffbb1949d0b1e19479a0c14bdfca5
Aashish Karki <078bct004.aashish@pcampus.edu.np>
2024-03-09 22:54:15 +0545

    The Drawer is now functional

--- src/components/Library/Library.jsx
+++ src/components/Library/Library.jsx
@@ -1,44 +1,36 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
-import { Drawer } from 'vaul';
-import './Library.scss';
+import React from "react";
+import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { Drawer } from "vaul";
+import "./Library.scss";
+import SongCard from "../SongCard/SongCard.jsx";
 
-const Library = (
-    {
-        songs,
-        setSongs,
-        libraryStatus,
-        setLibraryStatus,
-        // setIsInFavorites,
-        favoriteSongs }
-) => {
-    let color1 = '#fff';
-    let color2 = '#fff';
-    // songs.map(song => {
-    //     if (song.active) {
-    //         color1 = song.color[0];
-    //         color2 = song.color[1];
-    //     }
-    // });
-    const gradient = {
-        backgroundImage: `linear-gradient(to right, ${color1}, ${color2})`
-    };
-    return (
-        <div className={`${libraryStatus?'library-active':'library'}`}>
+const Library = ({ songs, setSongs, libraryStatus,setLibraryStatus, currentSong, setCurrentSong }) => {;
 
-        </div>
-    );
+  return (
+    <div className={`${libraryStatus ? "library-active" : "library"}`}>
+      <div className="library-header">
+        <h2>Songs</h2>
+        <button>Sort</button>
+      </div>
+
+      <div >
+        {songs&&songs.map((song, index) => (
+          <SongCard song={song} currentSong={currentSong} setCurrentSong={setCurrentSong} libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} key={index} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 Library.propTypes = {
-    songs: PropTypes.array,
-    setSongs: PropTypes.func,
-    libraryStatus: PropTypes.bool,
-    setLibraryStatus: PropTypes.func,
-    setIsInFavorites: PropTypes.func,
-    favoriteSongs: PropTypes.array
+  songs: PropTypes.array,
+  setSongs: PropTypes.func,
+  libraryStatus: PropTypes.bool,
+  setLibraryStatus: PropTypes.func,
+  setIsInFavorites: PropTypes.func,
+  favoriteSongs: PropTypes.array,
 };
 
 export default Library;
